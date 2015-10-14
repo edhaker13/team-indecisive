b7633555f41fe7affb379417d720c03934f75b5c
SskaaraCometh <noctisub-zero@hotmail.com>
2015-10-14 16:43:58 +0100

    Vertex Names changed and removed DirectX dependency

--- Structures.h
+++ Structures.h
@@ -1,42 +1,49 @@
-#include <d3d11_1.h>
-#include <d3dcompiler.h>
-#include <directxcolors.h>
-#include "resource.h"
-#include <vector>
 #include "DirectXMath.h"
 
-using namespace Indecisive;
-
-struct Geometry
-{
-	SimpleVertex* vertexBuffer;
-	unsigned short indexBuffer;
-	int numberOfIndices;
-
-	UINT vertexBufferStride;
-	UINT vertexBufferOffset;
-};
-
-struct Material
-{
-	Vector4 diffuse;
-	Vector4 ambient;
-	//------- LIGHTING NOT YET IMPLEMENTED
-
-	//Vector4 specular;
-	//float specularPower;
-
-	//-------
-};
-
-struct SimpleVertex
-{
-	Vector3 Pos;
-	Vector3 Normal;
-	Vector3 TexC;
-
-	bool operator<(const SimpleVertex other) const
+namespace Indecisive
+{ 
+	struct Geometry
 	{
-		return memcmp((void*)this, (void*)&other, sizeof(SimpleVertex)) > 0;
-	}
-};
+		Vertex* vertexBuffer;
+		unsigned short indexBuffer;
+		int numberOfIndices;
+
+		unsigned int vertexBufferStride;
+		unsigned int vertexBufferOffset;
+
+	};
+
+	struct Material
+	{
+		Vector4 diffuse;
+		Vector4 ambient;
+
+		//------- LIGHTING NOT YET IMPLEMENTED
+
+		//Vector4 specular;
+		//float specularPower;
+
+		//-------
+	};
+
+	struct Vertex
+	{
+		Vector3 Pos;
+		Vector3 Normal;
+		Vector3 TexC;
+
+		bool operator<(const Vertex other) const
+		{
+			return memcmp((void*)this, (void*)&other, sizeof(Vertex)) > 0;
+		}
+	};
+
+	struct SimpleVertex
+	{
+		Vector3 Pos;
+		Vector4 Colour;
+	};
+
+		
+
+}
