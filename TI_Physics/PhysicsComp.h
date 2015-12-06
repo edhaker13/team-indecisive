5178d82b6ee28d04c0282d31a16e5114cc34bac7
SskaaraCometh <noctisub-zero@hotmail.com>
2015-12-06 18:18:10 +0000

    DependInjection ItranformComponent to transform component

--- TI_Physics/PhysicsComp.h
+++ TI_Physics/PhysicsComp.h
@@ -1,14 +1,53 @@
 #pragma once
+#include "ITransformComponent.h"
+#include "TransformComponent.h"
 
 namespace Indecisive
 {
 	class PhysicsComp
 	{
 	public:
-		PhysicsComp();
+		PhysicsComp(ITransformComponent* _transform);
 		~PhysicsComp();
 
+		void MoveConstVel(float t);
+		void MoveConstAcc(float t);
+		void Move(float newX, float newY, float newZ);
+
+		//void SetVel(float3 _velocity) { velocity = _velocity; }
+
+		//float GetMass() const { return mass; };
+		//void SetMass(float _mass) { mass = _mass; }
+
+		//void UpdateNetForce();
+		//void UpdateAccel();
+		//void UpdateState();
+		//virtual void Update(float t);
+
+		//void SlidingMotion();
+		//void SlidingForce(float theta, float frCoef);
+
+		//void MotionInFluid(float t);
+		//void DragForce();
+		//void dragLamFlow();
+		//void TurbulantFlow();
+
+		////Collision
+		//float GetCollisionRadius() const { return radius; }
+		//void SetCollisionRadius(float _radius) { radius = _radius; }
+
+		//bool CubeCollisionCheck(float3 position, float radius);
+		//void FloorCollisionCheck(float3 position);
+		//void ResolveCollision(ParticleModel* particleModel1, ParticleModel* particleModel2, float restitution);
+
+		//void SetSlidingForce();
+
+		//void GetDirection();
+
 	protected:
+		/*float3 velocity;
+		float3 Acceleration;
+		ITransformComponent* _transform;*/
 
 
 	private:
--- TI_Physics/TransformComponent.h
+++ TI_Physics/TransformComponent.h
@@ -1,12 +1,31 @@
 #pragma once
+#include "ITransformComponent.h"
+
 namespace Indecisive
 {
 
-	class TransformHandler
+	class TransformComponent : public ITransformComponent
 	{
 	public:
-		TransformHandler();
-		~TransformHandler();
+		TransformComponent();
+		~TransformComponent();
+
+		//Setters and Getters for Pos/Scal/Rotat
+
+		//void SetPosition(float3 position) { _prevPos = _position; _position = position; }
+		//void SetPosition(float x, float y, float z) { _position.x = x; _position.y = y; _position.z = z; }
+		//float3 GetPrevPosition() const { return _prevPos; }
+		//float3 GetPosition() const { return _position; }
+
+		//void SetScale(float3 scale) { _scale = scale; }
+		//void SetScale(float x, float y, float z) {_scale.x = x; _scale.y = y; _scale.z = z;}
+		//float3 GetScale() const { return _scale }
+
+		//void SetRotation(float3 rotation) {_rotation = rotation;)
+		//void SetRotation(float x, float y, float z) {_rotation.x = x; _rotation.y = y; _rotation.z = z;}
+		//float3 GetRotation() const { return _rotation }
+
+		void Update(float t);
 
 	private:
 		/*float3 _position;
