c6cec8e40f1591ae4d2d8ea25dcd0b86271e8aa6
edhaker13 <luis@checa.xyz>
2015-12-17 15:56:50 +0000

    Fixed no visible output, window takes parameters.

--- TI_Engine/SceneGraph.h
+++ TI_Engine/SceneGraph.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <map>
 #include "DirectXMath.h"
-#include "IGameObject.h"
 
 namespace Indecisive
 {
+	class IGameObject;
 	/// <summary> Self contained n-ary tree. Expects a Matrix </summary> Polymorphism and recursion idea from http://www.andresilaghi.com/curious-projects/opengl-scene-graph-in-c/
 	struct TreeNode
 	{
@@ -12,193 +12,65 @@ namespace Indecisive
 		std::string key;
 		TreeNode* parent = nullptr;
 		Matrix world = Matrix::Identity;
-
-		TreeNode(): parent(nullptr) {};
-		TreeNode(std::string key) : key(key), parent(nullptr) {};
-		~TreeNode(void)
-		{
-			for (auto it = children.begin(); it != children.end();)
-			{
-				delete it->second;
-				it = children.erase(it);
-			}
-			if (parent != nullptr)
-			{
-				delete parent;
-				parent = nullptr;
-			}
-		};
-
-		void Append(TreeNode* appendix)
-		{
-			if (appendix != nullptr)
-			{
-				children.emplace(appendix->key, appendix);
-				appendix->parent = this;
-			}
-		};
-
+		TreeNode(const std::string& key) : key(key), parent(nullptr) {};
+		~TreeNode();
+		/// <summary> Adds the node as a child </summary>
+		LIBRARY_API void Append(TreeNode*);
 		/// <summary> Searches in all siblings first </summary>
-		static TreeNode* Find(TreeNode* node, const std::string& key)
-		{
-			if (node != nullptr)
-			{
-				if (node->key == key)
-				{
-					return node;
-				}
-				TreeNode* found = node->children.find(key)->second;
-				if (found == node->children.end()->second)
-				{
-					for (const auto& p: node->children)
-					{
-						found = Find(p.second, key);
-					}				
-				}
-				return found;
-			}
-			return nullptr;
-		};
-
+		static const TreeNode* Find(const TreeNode*, const std::string&);
 		/// <summary> Recursively draw children </summary>
-		virtual void Draw()
-		{
-			for (const auto& pair: children)
-			{
-				(pair.second)->Draw();
-			}
-		};
-
+		LIBRARY_API virtual void Draw();
 		/// <summary> Recursively update children </summary>
-		virtual void Update(float elapsedTime)
-		{
-			for(const auto& pair: children)
-			{
-				(pair.second)->Update(elapsedTime);
-			}
-			world *= GetParentWorld();
-		};
-
-		/// <summary> Recursively check collision on children </summary>
-		//virtual void Selection(std::vector<ObjectNode*>& hits, const float3& rStart, const float3& rEnd);
-
-		// <summary> Return this node's transformation matrix </summary>
-		virtual const Matrix GetWorld() { return world; };
-
-		// <summary> Return parent node's transformation matrix </summary>
-		virtual const Matrix GetParentWorld()
-		{
-			return parent != nullptr ? parent->GetWorld() : Matrix::Identity;
-		}
+		LIBRARY_API virtual void Update(float);
+		/// <summary> Return this node's transformation matrix </summary>
+		LIBRARY_API virtual const Matrix GetWorld();
+		/// <summary> Return parent node's transformation matrix </summary>
+		LIBRARY_API virtual const Matrix GetParentWorld();
 	};
-
+	/// <summary> Node containing a game object to use </summary>
 	struct ObjectNode : public TreeNode
 	{
 	private:
 		IGameObject& _object;
 	public:
-		ObjectNode(std::string key, IGameObject& object) : TreeNode(key), _object(object) {};
-		virtual void Draw() override { _object.Draw(); TreeNode::Draw(); };
-		virtual void Update(float elapsedTime) override
-		{
-			_object.Update(elapsedTime);
-			world = _object.GetWorld();
-			TreeNode::Update(elapsedTime); 
-		};
-		//virtual void Selection(std::vector<ObjectNode*>& hits, const float3& rStart, const float3& rEnd);
+		LIBRARY_API ObjectNode(const std::string& key, IGameObject& object) : TreeNode(key), _object(object) {};
+		LIBRARY_API virtual void Draw() override;
+		LIBRARY_API virtual void Update(float) override;
 	};
-
+	/// <summary> Node with a rotation to apply to all child objects </summary>
 	struct RotationNode : public TreeNode
 	{
-		float yaw, pitch, roll;
-
-		RotationNode(std::string key, float yaw, float pitch, float roll) : TreeNode(key), yaw(yaw), pitch(pitch), roll(roll) {};
-
-		virtual void Update(float elapsedTime) override
-		{
-			world = Matrix::CreateFromYawPitchRoll(yaw, pitch, roll);
-			TreeNode::Update(elapsedTime);
-		};
+		const float yaw, pitch, roll;
+		LIBRARY_API RotationNode(const std::string& key, float yaw, float pitch, float roll) : TreeNode(key), yaw(yaw), pitch(pitch), roll(roll) {};
+		LIBRARY_API virtual void Update(float) override;
 	};
-
+	/// <summary> Node with position to translate all child objects </summary>
 	struct PositionNode : public TreeNode
 	{
 		Vector3 position;
-		PositionNode(std::string key, const Vector3& position) : TreeNode(key), position(position) {};
-
-		virtual void Update(float elapsedTime) override
-		{
-			world = Matrix::CreateTranslation(position);
-			TreeNode::Update(elapsedTime);
-		};
+		LIBRARY_API PositionNode(const std::string& key, const Vector3& position) : TreeNode(key), position(position) {};
+		LIBRARY_API virtual void Update(float) override;
 	};
 	/// <summary> Node with position that will stay within the offsets of cameraEye </summary>
 	struct ClampedPositionNode : public PositionNode
 	{
 		const Vector3& cameraEye;
-		float lowerBound, upperBound;
-		ClampedPositionNode(std::string key, const Vector3& position, const Vector3& cameraEye, float lowerBound, float upperBound) :
+		const float lowerBound, upperBound;
+		LIBRARY_API ClampedPositionNode(const std::string& key, const Vector3& position, const Vector3& cameraEye, float lowerBound, float upperBound) :
 			PositionNode(key, position), cameraEye(cameraEye), lowerBound(lowerBound), upperBound(upperBound) {};
-
-		virtual void Update(float elapsedTime) override
-		{
-			if (position.z < lowerBound && position.z > upperBound)
-			{
-				position.z = (upperBound - lowerBound) / 2;
-			}
-			TreeNode::Update(elapsedTime);
-		}
+		LIBRARY_API virtual void Update(float) override;
 	};
-
-	/// <summary> Node with camera center, eye, and up positions </summary>
+	/// <summary> Node with camera eye, center, and up positions. Will follow center position </summary>
 	struct CameraNode : public TreeNode
 	{
-		Vector3 eye, up, distance;
-		float nearZ, farZ;
-
-		///<summary> Reference to position to follow as the center of the camera</summary>
-		Vector3& center;
-
-		LIBRARY_API CameraNode(std::string key, Vector3& center, const Vector3& eye, const Vector3& up, float nearZ, float farZ) :
-			TreeNode(key), center(center), eye(eye), up(up), distance(center - eye), nearZ(nearZ), farZ(farZ) {};
-
-		/// <summary> Loop around eye x and y if outside limits </summary>
-		void ClampEye()
-		{
-			const float limit = 20.0f;
-			if (eye.x > limit)
-			{
-				eye.x = (eye.x - limit) * -1.0f;
-				distance.x = eye.x - 1.0f;
-			}
-			else if (eye.x < -limit)
-			{
-				eye.x = (eye.x + limit) * -1.0f;
-				distance.x = eye.x + 1.0f;
-			}
-
-			if (eye.y > limit)
-			{
-				eye.y = (eye.y - limit) * -1.0f;
-				distance.y = eye.y - 1.0f;
-			}
-			else if (eye.y < -limit)
-			{
-				eye.y = (eye.y + limit) * -1.0f;
-				distance.y = eye.y + 1.0f;
-			}
-		};
-
+		///<summary> Position to follow as the center of the camera </summary>
+		const Vector3& center;
+		const Vector3 up, distance;
+		Vector3 eye;
+		const float nearZ, farZ;
+		LIBRARY_API CameraNode(std::string key, const Vector3& eye, const Vector3& center, const Vector3& up, float nearZ, float farZ) :
+			TreeNode(key), eye(eye), center(center), up(up), distance(center - eye), nearZ(nearZ), farZ(farZ) {};
 		/// <summary> Update eye position if the center has moved </summary>
-		virtual void Update(float elapsedTime) override
-		{
-			if (center + distance != eye)
-			{
-				eye = center - distance;
-			}
-			ClampEye();
-			TreeNode::Update(elapsedTime);
-		};
+		LIBRARY_API virtual void Update(float) override;
 	};
-
 };
