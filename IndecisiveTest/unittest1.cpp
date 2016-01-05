dd2fcef0557f820da931e835b2b8f81ec4c3dfd0
SskaaraCometh <noctisub-zero@hotmail.com>
2016-01-05 14:28:31 +0000

    Finished the 2 other test cases; textureloading and levelloading

--- IndecisiveTest/unittest1.cpp
+++ IndecisiveTest/unittest1.cpp
@@ -11,22 +11,26 @@ namespace Indecisive
 	{
 	private:
 		IGraphics* _pGraphics;
-	public:
+	public:	
+		
+		
+		TEST_METHOD(LevelLoaderTest)
+			{
+				auto lvlLoader = LevelLoader();
+				auto LV = LevelLoader();
+				
+				static const std::string level = "dummy.lvl";
+				bool CanRead = lvlLoader.CanRead(level);
+
+				auto root = static_cast<TreeNode*> (ResourceManagerInstance()->GetService("root"));
+				
+				Assert::IsTrue(CanRead);
+			};
+
+			
 			TEST_METHOD(ObjectMeshTest)
 			{
-				/* WHAT AM I EVEN
-				Window* pWindow = new Window(1280, 720, L"TEST");
-				
-				pWindow->Initialise(nullptr, 5);
 
-				assert(pWindow != nullptr);
-				
-				auto pGraphics = new GraphicsDirectX();
-				pGraphics->Initialise(pWindow);
-				ResourceManagerInstance()->AddService("graphics", pGraphics);
-				ResourceManagerInstance()->AddService("root", new TreeNode("root"));
-				ResourceManagerInstance()->AddService("camera", new CameraNode("cam", Vector3(0.f, 5.f, 100.f), Vector3::Zero, Vector3::Up, 0.0f, 100.0f));
-				IGameObject* GO = ComponentFactory::MakeObjectFromObj("fullcar.obj");*/
 				auto lvlLoader = LevelLoader();
 				static const std::string level = "dummy.lvl";
 				lvlLoader.ReadWindow(level);
@@ -35,27 +39,30 @@ namespace Indecisive
 				
 				auto root = static_cast<TreeNode*> (ResourceManagerInstance()->GetService("root"));
 				auto nodeObject = static_cast<ObjectNode*> (root->children["move"]->children["car"]);
-				auto GO = &nodeObject->Object();
+				auto GO = &nodeObject->Object(); 
 
-				//PROFIT???
 				Assert::IsNotNull(GO);
-
-				//Assert::IsNull(GO);
 
 			}
 
 			TEST_METHOD(TextureLoadTest)
 			{
-				/*Assert::IsTrue();*/
-			}
 
-			TEST_METHOD(LevelLoaderTest)
-			{
+				Window* pWindow = new Window(1280, 720, L"TEST"); 
+				pWindow->Initialise(nullptr, 5);
+
+				auto pGraphics = new GraphicsDirectX();
 				
-			};
+				ResourceManagerInstance()->AddService("graphics", pGraphics);
+				ResourceManagerInstance()->AddService("root", new TreeNode("root"));
+				ResourceManagerInstance()->AddService("camera", new CameraNode("cam", Vector3(0.f, 5.f, 100.f), Vector3::Zero, Vector3::Up, 0.0f, 100.0f));
+				pGraphics->Initialise(pWindow);
+				bool test = ResourceManagerInstance()->AddTexture("WhiteTex.dds");
+				
 
-			
-			
+				Assert::IsTrue(test);
+				
+			}
 		
 	};
 }
